873befda62ed9421debac0e1dc66dbdd749228f7
Takumikimura141899 <kimura141899@gmail.com>
2021-07-29 06:26:48 +0900

    fetch_ async/await

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
+  const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    console.log('useEffectが実行されました');
-  }, [count]);
+  // useEffect(() => {
+  //   // fetchメソッドで外部API(今回はJSONPlaceholderというテストに便利なAPI)と通信を行う
+  //   // 今回はpostsデータを取得してくる
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     // 帰ってきたhttp通信をjson形式に変換
+  //     .then((res) => res.json())
+  //     // その中のデータをsetPosts関数を使ってpostsにセットする
+  //     // .then((data) => console.log(data));
+  //     .then((data) => {
+  //       setPosts(data);
+  //     }, []);
+  // });
+
+  async function getDataByJson() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const json = await res.json();
+    console.log(json);
+    setPosts(json);
+  }
+
+  useEffect(() => getDataByJson(), []);
 
   return (
     <div className='App'>
       <h1>Learn useEffect</h1>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
-      <h2>Count2: {count2}</h2>
-      <button onClick={() => setCount2((prevCount) => prevCount + 1)}>+</button>
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>{post.title}</div>
+        ))}
+      </div>
     </div>
   );
 }
