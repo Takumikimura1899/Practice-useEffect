6938315112166bc19aa2459b6ac495d250e07fdb
Takumikimura141899 <kimura141899@gmail.com>
2021-07-30 04:50:47 +0900

    add  cleanUp function

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
+import Count from './components/Count.js';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  // useEffect(() => {
-  //   // fetchメソッドで外部API(今回はJSONPlaceholderというテストに便利なAPI)と通信を行う
-  //   // 今回はpostsデータを取得してくる
-  //   fetch('https://jsonplaceholder.typicode.com/posts')
-  //     // 帰ってきたhttp通信をjson形式に変換
-  //     .then((res) => res.json())
-  //     // その中のデータをsetPosts関数を使ってpostsにセットする
-  //     // .then((data) => console.log(data));
-  //     .then((data) => {
-  //       setPosts(data);
-  //     }, []);
-  // });
-
-  async function getDataByJson() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const json = await res.json();
-    console.log(json);
-    setPosts(json);
-  }
-
-  useEffect(() => getDataByJson(), []);
+  const [display, setDisplay] = useState(true);
 
   return (
     <div className='App'>
       <h1>Learn useEffect</h1>
-      <div>
-        {posts.map((post) => (
-          <div key={post.id}>{post.title}</div>
-        ))}
-      </div>
+      <button onClick={() => setDisplay(!display)}>Toggle</button>
+      {display && <Count />}
     </div>
   );
 }
